f281832c46c5f6f57c59b15ea4fdf57a47c08cd0
Aaron <Aaron.Slowinski@gmail.com>
2024-04-15 20:24:09 -0700

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(num):
+    for i in range(1, num + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Testing the function with an argument of 50
+fizzbuzz(50)
