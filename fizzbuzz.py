686b3ab113b385477a4746a2915e98c1f8f97049
Aaron <Aaron.Slowinski@gmail.com>
2024-04-23 21:09:33 -0700

    comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,17 @@
 def fizzbuzz(num):
+    # Loop through numbers from 1 to num
     for i in range(1, num + 1):
+        # Check if the number is divisible by both 3 and 5
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz")  # If divisible by both, print "FizzBuzz"
+        # Check if the number is divisible by 3
         elif i % 3 == 0:
-            print("Fizz")
+            print("Fizz")  # If divisible by 3, print "Fizz"
+        # Check if the number is divisible by 5
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz")  # If divisible by 5, print "Buzz"
         else:
-            print(i)
+            print(i)  # If not divisible by either, print the number itself
 
 # Testing the function with an argument of 50
 fizzbuzz(50)
