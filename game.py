52c525d1fbdc65c9e2fc0ef14e91cf85f82d4f93
Aaron <Aaron.Slowinski@gmail.com>
2024-04-25 19:37:49 -0700

    alien game

--- game.py
+++ game.py
@@ -6,6 +6,11 @@ class Alien (Sprite):
         super().__init__()
         self.screen = ai_game.screen
         
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('/images/alien.png')
         self.react=self.image.get_rect()
+        
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+        self.x = float (self.react.x)
         
