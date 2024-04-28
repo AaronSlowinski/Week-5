ca2fe7c2c4ff2bbad8fe32967622657224bb9f96
Aaron <Aaron.Slowinski@gmail.com>
2024-04-27 21:09:05 -0700

    game

--- game.py
+++ game.py
@@ -2,6 +2,8 @@
 
 import pygame
 from pygame.sprite import Sprite
+from bullet import Bullet
+from alien import Alien
 
 
 class Alien(Sprite):
@@ -31,3 +33,23 @@ class Alien(Sprite):
         
         self.x = float(self.rect.x)
         
+
+def __init__ (self):
+    self.ship = Ship(self)
+    self.bullets = pygame.sprite.Group()
+    self.aliens = pygame.sprite.Group()
+    
+    self._create_fleet()
+    
+def _create_fleet(self):
+    alien = Alien(self)
+    self.aliens.add(alien)
+
+def _update_screen(self):
+    self.screen.fill(self.settings.bg_color)
+    self.ship.blitme()
+    for bullet in self.bullets.sprites():
+        bullet.draw_bullet()
+    self.aliens.draw(self.screen)
+    
+    pygame.display.flip() 
